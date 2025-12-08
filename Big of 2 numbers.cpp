ba2c6cb6ad801db01bc3e5883b86e08c12ee1a26
yaswanthraju-7 <siriraju769@gmail.com>
2025-12-08 12:35:32 +0530

    Add files via upload

---
+++ Big of 2 numbers.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y;
+        scanf("%d", &X);
+            scanf("%d", &Y);
+
+                if (X >= Y) {
+                        printf("%d
+", X);
+                            } else {
+                                    printf("%d
+", Y);
+                                        }
+
+                                            return 0;
+                                            }
