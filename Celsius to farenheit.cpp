1dd64a4145660504a6705c1646e136772177eea4
yaswanthraju-7 <siriraju769@gmail.com>
2025-12-08 12:34:57 +0530

    Add files via upload

---
+++ Celsius to farenheit.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int C;
+        double F;
+
+            scanf("%d", &C);
+
+                F = (C * 9.0 / 5.0) + 32;
+
+                    printf("%.2f
+", F);
+
+                        return 0;
+                        }
