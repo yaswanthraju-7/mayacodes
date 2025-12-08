4349798445573032118c75e2ea77ed0167fdc99c
yaswanthraju-7 <siriraju769@gmail.com>
2025-12-08 11:37:49 +0530

    Add files via upload

---
+++ 1d-array.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int marks[6]={89,90,76,78,98,86};
+	printf("%d \n",marks[0]);
+	printf("%d \n",marks[1]);
+	printf("%d \n",marks[2]);
+	printf("%d \n",marks[3]);
+	printf("%d \n",marks[4]);
+	printf("%d \n",marks[5]);
+}
