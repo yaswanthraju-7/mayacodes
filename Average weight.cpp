9e3bf2d6a3a99219d3436298150af4073f5e6a67
yaswanthraju-7 <siriraju769@gmail.com>
2025-12-08 12:24:11 +0530

    Add files via upload

---
+++ Average weight.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int avg, w1, w2, w3;
+        scanf("%d %d %d", &avg, &w1, &w2);
+            w3 = (3 * avg) - (w1 + w2);
+                printf("%d", w3);
+                    return 0;
+                    }
