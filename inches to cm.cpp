7963c91203edc4281806e2693be2c48ce1559587
yaswanthraju-7 <siriraju769@gmail.com>
2025-12-08 12:23:13 +0530

    Add files via upload

---
+++ inches to cm.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int height_in_inches;
+    scanf("%d",&height_in_inches);
+    float height_in_cm = height_in_inches*2.54;
+    printf("%.2f",height_in_cm);
+    return 0;
+}
