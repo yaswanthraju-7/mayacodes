d5dbad7366394d353d592926fc0af736164c9678
yaswanthraju-7 <siriraju769@gmail.com>
2025-12-08 12:03:25 +0530

    Add files via upload

---
+++ Divisible with 9.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int input;
+    scanf("%d", &input);
+    if(input % 9 == 0)
+    {
+        printf("True
+");
+    }
+    else
+    {
+        printf("False
+");
+    }
+    return 0;
+}
