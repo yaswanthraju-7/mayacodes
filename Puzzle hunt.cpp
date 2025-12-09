8d236c711878ab2b6c13fbdc6cee9efef96e0495
yaswanthraju-7 <siriraju769@gmail.com>
2025-12-09 06:24:29 +0530

    Add files via upload

---
+++ Puzzle hunt.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    if (N >= 6 && N <= 8)
+    {
+        printf("Yes
+");
+    }
+    else{
+        printf("No
+");
+    }
+    return 0;
+}
